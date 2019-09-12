c78bf395346acca66d12b436af2f62163349585e
Anton Kucherov <anton.kucherov@percona.com>
2019-09-12 13:15:49 +0300

    PMM-4417: Update pmm-client image version.

--- pmm-app/src/pmm-easy-button-panel/module.ts
+++ pmm-app/src/pmm-easy-button-panel/module.ts
@@ -16,7 +16,7 @@ export class PanelCtrl extends MetricsPanelCtrl {
         status: "Scale",
         clusterName: "my-cluster",
         operatorImage: "perconalab/percona-xtradb-cluster-operator:PR-237-2d1eb9d",
-        pmmImage: "perconalab/pmm-client-fb:PR-410-135bd4b",
+        pmmImage: "perconalab/pmm-client-fb:PR-410-603b8ac",
         pmmHost: "monitoring-service:443",
     };
 
